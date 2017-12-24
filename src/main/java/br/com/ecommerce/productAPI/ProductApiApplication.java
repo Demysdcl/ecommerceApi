6ea16237366bd8c46a5ec9f21e98fbe97899142a
Demys <demys_dcl@hotmail.com>
2017-12-24 14:33:24 -0400

    structure made

---
+++ src/main/java/br/com/ecommerce/productAPI/ProductApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.ecommerce.productAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+}
